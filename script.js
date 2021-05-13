88de65c981203d327ef85a57483be8d38baf162b
Andre Alencar <aaugusto310@gmail.com>
2021-05-13 11:05:15 -0300

    inclusão de novas funcionalidades.

--- script.js
+++ script.js
@@ -5,8 +5,22 @@ for (let btn of buttons) {
   btn.addEventListener('click', () => {
     let cmd = btn.dataset['command'];
     if (cmd === 'createlink') {
-      let url = prompt("Enter the link here: ", "http:\/\/");
+      let url = prompt("Informe o link: ", "http:\/\/");
       document.execCommand(cmd, false, url);
+    } else {
+      document.execCommand(cmd, false, null);
+    }
+
+    if (cmd === 'fontSize') {
+      let size = prompt("Informe o tamanho entre 1-7: ", 3);
+      document.execCommand(cmd, false, size);
+    } else {
+      document.execCommand(cmd, false, null);
+    }
+
+    if (cmd === 'formatBlock') {
+      let tag = prompt("Informe a tag HTML que deseja formatar: ", "<p>");
+      document.execCommand(cmd, false, tag);
     } else {
       document.execCommand(cmd, false, null);
     }
